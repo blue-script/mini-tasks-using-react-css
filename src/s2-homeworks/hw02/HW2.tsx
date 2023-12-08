001391661ba4cc3aaf705fbeea963dbcabc60782
demianov-an <Alexm92@inbox.ru>
2023-12-08 14:57:18 +0600

    homework-2 fixed props in Affairs.tsx file

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -9,7 +9,7 @@ import s2 from '../../s1-main/App.module.css'
 * 4 - выполнить пункт 3 для функции deleteAffair+
 * 5 - указать нужный тип в useState с affairs +
 * 6 - дописать тип и логику функции deleteAffairCallback+
-* 7 - в файле Affairs.tsx дописать типизацию пропсов
+* 7 - в файле Affairs.tsx дописать типизацию пропсов +
 * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
 * 9 - в файле Affair.tsx дописать типизацию пропсов
 * 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
