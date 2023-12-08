5733385d5de46bf6d8587c2d80c19bf83100b3c1
demianov-an <Alexm92@inbox.ru>
2023-12-08 15:20:39 +0600

    homework-2 fixed function deleteCallback and input-data in Affairs.tsx file

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -11,9 +11,9 @@ import s2 from '../../s1-main/App.module.css'
 * 6 - дописать тип и логику функции deleteAffairCallback+
 * 7 - в файле Affairs.tsx дописать типизацию пропсов +
 * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow +
-* 9 - в файле Affair.tsx дописать типизацию пропсов
-* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
-* 11 - в файле Affair.tsx отобразить приходящие данные
+* 9 - в файле Affair.tsx дописать типизацию пропсов +
+* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать +
+* 11 - в файле Affair.tsx отобразить приходящие данные +
 * */
 
 // types
--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -11,7 +11,7 @@ type AffairPropsType = {
 
 function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
-        // need to fix
+        props.deleteAffairCallback(props.affair._id)
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -26,7 +26,7 @@ function Affair(props: AffairPropsType) {
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
                 {/*создаёт студент*/}
 
-                {/**/}
+                {props.affair.name}
             </div>
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {props.affair.priority}
@@ -36,7 +36,7 @@ function Affair(props: AffairPropsType) {
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
                 // need to fix
-
+                onClick={deleteCallback}
             >
                 {/*текст кнопки могут изменить студенты*/}
                 X
