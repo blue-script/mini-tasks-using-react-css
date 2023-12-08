19538ea06a609b1160cb00a16f4bcc15892fe3e2
demianov-an <Alexm92@inbox.ru>
2023-12-08 15:00:54 +0600

    homework-2 fixed functions setAll, setHigh, setMiddle, setLow in Affairs.tsx file

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -10,7 +10,7 @@ import s2 from '../../s1-main/App.module.css'
 * 5 - указать нужный тип в useState с affairs +
 * 6 - дописать тип и логику функции deleteAffairCallback+
 * 7 - в файле Affairs.tsx дописать типизацию пропсов +
-* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
+* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow +
 * 9 - в файле Affair.tsx дописать типизацию пропсов
 * 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
 * 11 - в файле Affair.tsx отобразить приходящие данные
