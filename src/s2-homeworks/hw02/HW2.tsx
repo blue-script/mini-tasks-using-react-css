227178efdb2e4d178ee6dc158a180b71371d5186
demianov-an <Alexm92@inbox.ru>
2023-12-08 14:52:28 +0600

    homework-2 fixed function deleteAffairCallback

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -7,8 +7,8 @@ import s2 from '../../s1-main/App.module.css'
 * 2 - указать нужный тип для defaultAffairs +
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами +
 * 4 - выполнить пункт 3 для функции deleteAffair+
-* 5 - указать нужный тип в useState с affairs
-* 6 - дописать тип и логику функции deleteAffairCallback
+* 5 - указать нужный тип в useState с affairs +
+* 6 - дописать тип и логику функции deleteAffairCallback+
 * 7 - в файле Affairs.tsx дописать типизацию пропсов
 * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
 * 9 - в файле Affair.tsx дописать типизацию пропсов
@@ -47,8 +47,8 @@ function HW2() {
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
+    const deleteAffairCallback = (_id: number) => { // need to fix any
+        setAffairs(deleteAffair(affairs, _id))
     }
 
     return (
