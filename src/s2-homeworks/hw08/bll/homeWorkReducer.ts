212b3323b2bba10e1687ec901c71878b677c0bba
demianov-an <alexm92@inbox.ru>
2024-01-26 12:57:16 +0600

    homework-8 did homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return action.payload === 'up'
+              ? state // need to fix
+                .sort((a,b) => a.name.localeCompare(b.name))
+              : state // need to fix
+                .sort((a,b) => b.name.localeCompare(a.name))
         }
         case 'check': {
-
-            return state // need to fix
+            return state   // need to fix
+              .filter(u=> u.age >= action.payload )
+              .sort((a,b) => a.age - b.age )
         }
         default:
             return state
