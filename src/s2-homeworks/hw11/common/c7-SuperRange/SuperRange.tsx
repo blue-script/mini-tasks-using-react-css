02892de0fc681a23de6d013594c9c7d5891e3ed0
blue-script <alexm92@inbox.ru>
2024-03-01 14:55:26 +0500

    3 - сделать стили в соответствии с дизайном - пока что без точки в центре thumb

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,32 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 160,
+                color: '#0c2',
+                '& .MuiSlider-thumb': {
+                    // position: 'relative',
+                    // '&::before': {
+                    //     content: "''",
+                    //     position: 'absolute',
+                    //     top: '50%',
+                    //     left: '50%',
+                    //     transform: 'translate(-50%, -50%)',
+                    //     width: '8px',
+                    //     height: '8px',
+                    //     borderRadius: '50%',
+                    //     backgroundColor: 'blue',
+                    // },
+                    // '& .MuiSlider-thumb:last-child': {
+                    //     transform: 'translate(50%, -50%)',
+                    // },
+
+                    border: '1px solid #0c2',
+                    color: 'white',
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8b8b8b'
+                },
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
